c1555ed652f0d62c28ebddc2208b99561ae73748
AlexandreBessard <alexandre.bessard@gmail.com>
2021-05-24 16:13:09 -0400

    get description using repo

--- src/main/java/jpa/hibernate/repositories/CategoryRepository.java
+++ src/main/java/jpa/hibernate/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package jpa.hibernate.repositories;
 import jpa.hibernate.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/jpa/hibernate/repositories/UnitOfMeasureRepository.java
+++ src/main/java/jpa/hibernate/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package jpa.hibernate.repositories;
 import jpa.hibernate.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
